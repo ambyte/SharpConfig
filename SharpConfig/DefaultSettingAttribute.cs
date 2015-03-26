75a48a42041a0124c38a7c2eb178b99c7f86a9e8
Sergey Gulyaev <astraway@gmail.com>
2015-03-26 15:49:32 +0300

    added attribute classes for object properties settings

---
+++ SharpConfig/DefaultSettingAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpConfig
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class DefaultSettingAttribute : Attribute
+    {
+        private readonly object _value;
+
+        public DefaultSettingAttribute(object value)
+        {
+            _value = value;
+        }
+
+        public DefaultSettingAttribute(object value, string comment)
+        {
+            _value = value;
+        }
+
+        public object Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+    }
+}
---
+++ SharpConfig/DescriptionSettingAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpConfig
+{
+    public sealed class DescriptionSettingAttribute : Attribute
+    {
+        private readonly string _description;
+
+        public DescriptionSettingAttribute(string description)
+        {
+            _description = description;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+        }
+    }
+}
